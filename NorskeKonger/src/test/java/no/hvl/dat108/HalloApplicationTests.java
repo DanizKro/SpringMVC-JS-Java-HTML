5ad4c057c0af299dd2486058a3575c331898dd3f
186480 <186480@stud.hvl.no>
2025-10-10 13:02:00 +0200

    op

--- NorskeKonger/src/test/java/no/hvl/dat108/HalloApplicationTests.java
+++ NorskeKonger/src/test/java/no/hvl/dat108/HalloApplicationTests.java
@@ -1,10 +1,10 @@
-package com.konger.NorskeKonger;
+package no.hvl.dat108;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NorskeKongerApplicationTests {
+class AppMainTests {
 
 	@Test
 	void contextLoads() {
